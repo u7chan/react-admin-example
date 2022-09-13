d185bfb53ed81f0c71aa376c97f905cd8725e211
una <unagamix@gmail.com>
2022-09-14 00:11:04 +0900

    feat: rowClick edit

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -4,7 +4,7 @@ import { List, Datagrid, TextField } from 'react-admin'
 export const PostList: FC = () => {
   return (
     <List>
-      <Datagrid>
+      <Datagrid rowClick="edit">
         <TextField source="id" />
         <TextField source="userId" />
         <TextField source="title" />
