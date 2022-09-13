c9e78bb3bd9c538a768afd5ecb1c95aeb62565f5
una <unagamix@gmail.com>
2022-09-13 23:10:42 +0900

    feat: add ra-language-japanese

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Admin, Resource, ListGuesser } from 'react-admin';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import japaneseMessages from '@bicstone/ra-language-japanese';
+
+const i18nProvider = polyglotI18nProvider(() => japaneseMessages, 'ja');
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
     <Resource name="posts" list={ListGuesser} />
     <Resource name="comments" list={ListGuesser} />
   </Admin>
